91dfd1bcc40ea822580e687c0865ac11268b81bf
Roushan Kumar <rk04393@gmail.com>
2023-10-01 23:52:40 +0530

    Java programs

--- src/com/rocky/Hello.java
+++ src/com/rocky/Hello.java
@@ -2,6 +2,7 @@ package com.rocky;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.print("Hello World");
+        System.out.println("Hello World");
+        System.out.println("My name is Roushan");
     }
 }
