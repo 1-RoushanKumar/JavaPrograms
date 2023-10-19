7aa892b24fcf0bcb1e45ddaeb749589721dbd416
Roushan Kumar <rk04393@gmail.com>
2023-10-19 08:26:21 +0530

    Java programs

--- src/com/questions/_32_RotateArray.java
+++ src/com/questions/_32_RotateArray.java
@@ -7,7 +7,7 @@ package com.questions;
 
 import java.util.Arrays;
 
-public class _32_RotateArry {
+public class _32_RotateArray {
     public static void main(String[] args) {
         int[] arr = {1, 2, 3, 4, 5};
         int k = 401;
