4f2a607e073f663f330ac1877cdd73fd4c0cb38f
Roushan Kumar <rk04393@gmail.com>
2023-12-25 11:36:08 +0530

    Java programs

--- src/com/_3_Arrays/_01_Arrays.java
+++ src/com/_3_Arrays/_01_Arrays.java
@@ -3,7 +3,7 @@ package com._3_Arrays;
 public class _01_Arrays {
     public static void main(String[] args) {
         int[] arr = new int[5];//array declaration
-        int[] arr1 = {5, 6, 8, 9, 0}; //if we dont add any value in array it will store 0 default.
+        int[] arr1 = {5, 6, 8, 9, 0}; //if we don't add any value in array it will store 0 default.
         System.out.println(arr1[0]);
         System.out.println(arr1[4]);
 //        System.out.println(arr1[5]);  //it will show array out of bound error because there are 5th index in array.
